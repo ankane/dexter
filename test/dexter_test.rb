b246da26cd9612a416fb036a57f18cbba0fdfd3c
Andrew Kane <acekane1@gmail.com>
2017-06-30 20:02:59 -0700

    Make assert private

--- test/dexter_test.rb
+++ test/dexter_test.rb
@@ -5,6 +5,8 @@ class DexterTest < Minitest::Test
     assert_index "SELECT * FROM posts WHERE id = 1", "posts (id)"
   end
 
+  private
+
   def assert_index(statement, index)
     dexter = Dexter::Client.new(["dexter_test", "-s", statement])
     assert_output(/Index found: #{Regexp.escape(index)}/) { dexter.perform }

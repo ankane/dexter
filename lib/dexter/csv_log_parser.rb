4db863bd5db32594afd18a59cf30d70f329b2a00
Andrew <acekane1@gmail.com>
2017-12-29 13:04:57 -0500

    Use constant [skip ci]

--- lib/dexter/csv_log_parser.rb
+++ lib/dexter/csv_log_parser.rb
@@ -2,12 +2,14 @@ require "csv"
 
 module Dexter
   class CsvLogParser < LogParser
+    FIRST_LINE_REGEX = /\A.+/
+
     def perform
       CSV.new(@logfile).each do |row|
         if (m = REGEX.match(row[13]))
           # replace first line with match
           # needed for multiline queries
-          active_line = row[13].sub(/\A.+/, m[3])
+          active_line = row[13].sub(FIRST_LINE_REGEX, m[3])
 
           add_parameters(active_line, row[14]) if row[14]
           process_entry(active_line, m[1].to_f)

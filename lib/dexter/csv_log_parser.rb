31473ba141810045ea2a544b13b58d70905a9a46
Andrew Kane <andrew@ankane.org>
2023-03-26 21:28:27 -0700

    DRY code [skip ci]

--- lib/dexter/csv_log_parser.rb
+++ lib/dexter/csv_log_parser.rb
@@ -6,17 +6,21 @@ module Dexter
 
     def perform
       CSV.new(@logfile.to_io).each do |row|
-        if (m = REGEX.match(row[13]))
-          # replace first line with match
-          # needed for multiline queries
-          active_line = row[13].sub(FIRST_LINE_REGEX, m[3])
-
-          add_parameters(active_line, row[14]) if row[14]
-          process_entry(active_line, m[1].to_f)
-        end
+        process_csv_row(row[13], row[14])
       end
     rescue CSV::MalformedCSVError => e
       raise Dexter::Abort, "ERROR: #{e.message}"
+    end
+
+    def process_csv_row(message, detail)
+      if (m = REGEX.match(message))
+        # replace first line with match
+        # needed for multiline queries
+        active_line = message.sub(FIRST_LINE_REGEX, m[3])
+
+        add_parameters(active_line, detail) if detail
+        process_entry(active_line, m[1].to_f)
+      end
     end
   end
 end
--- lib/dexter/log_table_parser.rb
+++ lib/dexter/log_table_parser.rb
@@ -1,21 +1,11 @@
 module Dexter
-  class LogTableParser < LogParser
-    FIRST_LINE_REGEX = /\A.+/
-
+  class LogTableParser < CsvLogParser
     def perform
       last_log_time = Time.at(0).iso8601(3)
 
       loop do
         @logfile.log_activity(last_log_time).each do |row|
-          if (m = REGEX.match(row["message"]))
-            # replace first line with match
-            # needed for multiline queries
-            active_line = row["message"].sub(FIRST_LINE_REGEX, m[3])
-
-            add_parameters(active_line, row["detail"]) if row["detail"]
-            process_entry(active_line, m[1].to_f)
-          end
-
+          process_csv_row(row["message"], row["detail"])
           last_log_time = row["log_time"]
         end
 

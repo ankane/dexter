94aa85fbc49360ec50268f68ef0d24c8a223535d
Andrew Kane <andrew@ankane.org>
2025-05-29 18:19:07 -0700

    Moved parsers to subdirectory [skip ci]

--- lib/dexter.rb
+++ lib/dexter.rb
@@ -20,13 +20,13 @@ require_relative "dexter/version"
 
 # parsers
 require_relative "dexter/log_parser"
-require_relative "dexter/csv_log_parser"
-require_relative "dexter/json_log_parser"
-require_relative "dexter/pg_stat_activity_parser"
-require_relative "dexter/pg_stat_statements_parser"
-require_relative "dexter/sql_log_parser"
-require_relative "dexter/statement_parser"
-require_relative "dexter/stderr_log_parser"
+require_relative "dexter/parsers/csv_log_parser"
+require_relative "dexter/parsers/json_log_parser"
+require_relative "dexter/parsers/pg_stat_activity_parser"
+require_relative "dexter/parsers/pg_stat_statements_parser"
+require_relative "dexter/parsers/sql_log_parser"
+require_relative "dexter/parsers/statement_parser"
+require_relative "dexter/parsers/stderr_log_parser"
 
 module Dexter
   class Abort < StandardError; end

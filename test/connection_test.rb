6fe39192b44d77895e1e0d0abe9369b61cb0da2d
Andrew Kane <andrew@ankane.org>
2025-06-01 04:23:43 -0700

    Added more connection tests

--- test/connection_test.rb
+++ test/connection_test.rb
@@ -17,10 +17,29 @@ class ConnectionTest < Minitest::Test
     assert_connection "postgresql:///dexter_test"
   end
 
+  def test_host
+    assert_connection_error "could not translate host name", "-h", "bad"
+  end
+
+  def test_port
+    assert_connection_error "5433", "-p", "5433"
+  end
+
+  def test_user
+    assert_connection_error %{role "bad" does not exist}, "-U", "bad"
+  end
+
   private
 
   def assert_connection(*args)
     output = run_command(*args, "-s", "SELECT 1", add_conninfo: false)
     assert_match "No new indexes found", output
+  end
+
+  def assert_connection_error(expected, *args)
+    error = assert_raises(Dexter::Error) do
+      assert_connection(*args)
+    end
+    assert_match expected, error.message
   end
 end

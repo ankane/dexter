d9f9208304e5621ccf1d21e0be6f2a9c6c50479f
Andrew Kane <andrew@ankane.org>
2025-06-01 03:10:43 -0700

    Fixed CI

--- test/input_test.rb
+++ test/input_test.rb
@@ -10,7 +10,7 @@ class InputTest < Minitest::Test
   end
 
   def test_csv_invalid
-    assert_error "ERROR: Illegal quoting", support_path("queries.json"), "--input-format", "csv"
+    assert_error "Illegal quoting", support_path("queries.json"), "--input-format", "csv"
   end
 
   def test_json
@@ -18,7 +18,7 @@ class InputTest < Minitest::Test
   end
 
   def test_json_invalid
-    assert_error "ERROR: unexpected token", support_path("queries.log"), "--input-format", "json"
+    assert_error "unexpected token", support_path("queries.log"), "--input-format", "json"
   end
 
   def test_sql

1f9ce736a59b49ebe9116c553cdd803de7db3693
Andrew Kane <andrew@ankane.org>
2025-05-31 21:35:46 -0700

    Added test for many columns [skip ci]

--- test/batching_test.rb
+++ test/batching_test.rb
@@ -3,14 +3,13 @@ require_relative "test_helper"
 class BatchingTest < Minitest::Test
   def setup
     super
-    skip unless ENV["TEST_BATCHING"]
   end
 
   def test_batching
+    skip unless ENV["TEST_BATCHING"]
+
     nc = 100
-    execute "DROP TABLE IF EXISTS t"
-    execute "CREATE TABLE t (#{nc.times.map { |i| "c%02d int" % i }.join(", ")})"
-    execute "INSERT INTO t SELECT #{nc.times.map { "n" }.join(", ")} FROM generate_series(1, 2000) n"
+    create_table(nc)
 
     queries = []
     nc.times do |i|
@@ -29,5 +28,22 @@ class BatchingTest < Minitest::Test
     output = run_command(tempfile.path, "--input-format", "sql", "--log-level", "debug2")
     assert_equal nc, output.scan(/Index found/).size
     assert_match "Batches: 50", output
+  end
+
+  def test_many_columns
+    nc = 100
+    create_table(nc)
+
+    statement =  "SELECT * FROM t WHERE #{nc.times.map { |i| "c%02d = 1" % i }.join(" AND ")}"
+    # TODO fix
+    assert_error "hypopg: not more oid available", "-s", statement
+  end
+
+  private
+
+  def create_table(nc)
+    execute "DROP TABLE IF EXISTS t"
+    execute "CREATE TABLE t (#{nc.times.map { |i| "c%02d int" % i }.join(", ")})"
+    execute "INSERT INTO t SELECT #{nc.times.map { "n" }.join(", ")} FROM generate_series(1, 2000) n"
   end
 end
